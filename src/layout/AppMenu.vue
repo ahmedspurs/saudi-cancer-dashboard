49c53efeb4f9c8db4f965d359a5282968b440275
ahmedspurs <amnnn80@gmail.com>
2025-07-19 10:43:58 +0300

    Fix Data in users

--- src/layout/AppMenu.vue
+++ src/layout/AppMenu.vue
@@ -8,80 +8,80 @@ const model = ref([
         label: 'لوحة التحكم',
         icon: 'pi pi-home',
         items: [
-            { label: 'الرئيسية', icon: 'pi pi-home', to: '/dashboard' },
-            { label: 'الإحصائيات', icon: 'pi pi-chart-bar', to: '/statistics' }
+            { label: 'الرئيسية', icon: 'pi pi-home', to: '/dashboard' }
+            // { label: 'الإحصائيات', icon: 'pi pi-chart-bar', to: '/statistics' }
         ]
     },
 
-    {
-        label: ' الحالات',
-        icon: 'pi pi-user-md',
-        items: [
-            { label: 'قائمة الحالات', icon: 'pi pi-users', to: '/cases' },
-            { label: '  اضافة حالة جديد', icon: 'pi pi-user-plus', to: '/add-case' }
-        ]
-    },
+    // {
+    //     label: ' الحالات',
+    //     icon: 'pi pi-user-md',
+    //     items: [
+    //         { label: 'قائمة الحالات', icon: 'pi pi-users', to: '/cases' },
+    //         { label: '  اضافة حالة جديد', icon: 'pi pi-user-plus', to: '/add-case' }
+    //     ]
+    // },
 
-    {
-        label: 'التبرعات',
-        icon: 'pi pi-heart',
-        items: [
-            { label: 'كل التبرعات', icon: 'pi pi-table', to: '/donations' },
-            {
-                label: 'أنواع التبرعات',
-                icon: 'pi pi-share-alt',
-                items: [
-                    { label: 'تبرعات الإهداءات', icon: 'pi pi-gift', to: '/gift-donations' },
-                    { label: 'تبرعات الحالات', icon: 'pi pi-user-plus', to: '/cases-donations' }
-                ]
-            }
-        ]
-    },
+    // {
+    //     label: 'التبرعات',
+    //     icon: 'pi pi-heart',
+    //     items: [
+    //         { label: 'كل التبرعات', icon: 'pi pi-table', to: '/donations' },
+    //         {
+    //             label: 'أنواع التبرعات',
+    //             icon: 'pi pi-share-alt',
+    //             items: [
+    //                 { label: 'تبرعات الإهداءات', icon: 'pi pi-gift', to: '/gift-donations' },
+    //                 { label: 'تبرعات الحالات', icon: 'pi pi-user-plus', to: '/cases-donations' }
+    //             ]
+    //         }
+    //     ]
+    // },
 
-    {
-        label: 'المدفوعات',
-        icon: 'pi pi-credit-card',
-        items: [{ label: 'سجلات الدفع', icon: 'pi pi-wallet', to: '/payments' }]
-    },
+    // {
+    //     label: 'المدفوعات',
+    //     icon: 'pi pi-credit-card',
+    //     items: [{ label: 'سجلات الدفع', icon: 'pi pi-wallet', to: '/payments' }]
+    // },
 
-    {
-        label: 'التقارير',
-        icon: 'pi pi-chart-line',
-        items: [
-            { label: 'تقرير التبرعات', icon: 'pi pi-file', to: '/donations-reports' },
-            { label: 'تقرير الحالات', icon: 'pi pi-user', to: '/cases-reports' },
-            { label: 'تقرير المدفوعات', icon: 'pi pi-money-bill', to: '/payments-reports' }
-        ]
-    },
+    // {
+    //     label: 'التقارير',
+    //     icon: 'pi pi-chart-line',
+    //     items: [
+    //         { label: 'تقرير التبرعات', icon: 'pi pi-file', to: '/donations-reports' },
+    //         { label: 'تقرير الحالات', icon: 'pi pi-user', to: '/cases-reports' },
+    //         { label: 'تقرير المدفوعات', icon: 'pi pi-money-bill', to: '/payments-reports' }
+    //     ]
+    // },
 
-    {
-        label: 'الإدارة',
-        icon: 'pi pi-cog',
-        items: [
-            {
-                label: 'المستخدمين والصلاحيات',
-                icon: 'pi pi-users',
-                items: [
-                    { label: 'قائمة المستخدمين', icon: 'pi pi-user', to: '/users' },
-                    { label: 'إضافة مستخدم', icon: 'pi pi-user-plus', to: '/add-user' }
-                    // { label: 'إدارة الصلاحيات', icon: 'pi pi-lock', to: '/roles' }
-                ]
-            },
-            {
-                label: 'الإعدادات',
-                icon: 'pi pi-sliders-h',
-                items: [
-                    { label: 'روابط التكامل', icon: 'pi pi-link', to: '/integration-links' },
-                    { label: 'تصنيفات التبرعات', icon: 'pi pi-tags', to: '/donation-categories' }
-                ]
-            },
-            {
-                label: 'الرسائل',
-                icon: 'pi pi-envelope',
-                items: [{ label: 'رسائل التواصل', icon: 'pi pi-inbox', to: '/contact-messages' }]
-            }
-        ]
-    },
+    // {
+    //     label: 'الإدارة',
+    //     icon: 'pi pi-cog',
+    //     items: [
+    //         {
+    //             label: 'المستخدمين والصلاحيات',
+    //             icon: 'pi pi-users',
+    //             items: [
+    //                 { label: 'قائمة المستخدمين', icon: 'pi pi-user', to: '/users' },
+    //                 { label: 'إضافة مستخدم', icon: 'pi pi-user-plus', to: '/add-user' }
+    //                 // { label: 'إدارة الصلاحيات', icon: 'pi pi-lock', to: '/roles' }
+    //             ]
+    //         },
+    //         {
+    //             label: 'الإعدادات',
+    //             icon: 'pi pi-sliders-h',
+    //             items: [
+    //                 { label: 'روابط التكامل', icon: 'pi pi-link', to: '/integration-links' },
+    //                 { label: 'تصنيفات التبرعات', icon: 'pi pi-tags', to: '/donation-categories' }
+    //             ]
+    //         },
+    //         {
+    //             label: 'الرسائل',
+    //             icon: 'pi pi-envelope',
+    //             items: [{ label: 'رسائل التواصل', icon: 'pi pi-inbox', to: '/contact-messages' }]
+    //         }
+    //     ]
+    // },
     {
         label: 'الموقع الالكتروني',
         icon: 'pi pi-globe',
